359e9f9ba108a50daa127189b34c62c8808c4816
Ansh Dwivedi <anshdhardwivediad@gmail.com>
2025-08-09 17:36:51 +0530

    bug

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 import express from "express";
 import "dotenv/config";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
 import cors from "cors";
 
 import userRoutes from "./routes/userRoutes.js";
 import ownerRoute from "./routes/ownerRoutes.js";
 import bookingRoute from "./routes/bookingRoutes.js";
 
- connectDB();
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/user', userRoutes);
-app.use('/api/owner', ownerRoute);
-app.use('/api/bookings', bookingRoute);
+app.use("/api/user", userRoutes);
+app.use("/api/owner", ownerRoute);
+app.use("/api/bookings", bookingRoute);
 
 const PORT = process.env.PORT || 4000;
+app.get("/", () => console.log("Server is running"));
 app.listen(PORT, () => {
   console.log(`Server is running at Port  ${PORT} `);
 });
