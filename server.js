ccb713f405c21ed34b29a40e4b8c9a1621c86679
Ansh Dwivedi <anshdhardwivediad@gmail.com>
2025-08-09 17:35:05 +0530

    bug fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/owner', ownerRoute);
 app.use('/api/bookings', bookingRoute);
-PORT=3000
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running at Port  ${PORT} `);
